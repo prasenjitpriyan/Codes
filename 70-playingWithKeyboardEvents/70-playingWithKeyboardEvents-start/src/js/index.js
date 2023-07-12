907b60d676987e88e2cae458d46e5215b588d9d8
Prasenjit Das <prasenjitpriyan@gmail.com>
2023-07-12 19:43:22 +0530

    New file added

---
+++ 70-playingWithKeyboardEvents/70-playingWithKeyboardEvents-start/src/js/index.js
@@ -0,0 +1,80 @@
+const bug = document.querySelector('.ladybird');
+const upBtn = document.querySelector('#up');
+const downBtn = document.querySelector('#down');
+const rightBtn = document.querySelector('#right');
+const leftBtn = document.querySelector('#left');
+const homeBtn = document.querySelector('#home');
+const turnClockwiseBtn = document.querySelector('#turnClockWise');
+const turnAntiClockwiseBtn = document.querySelector('#turnAntiClockWise');
+
+let rotation = 0;
+let xPos = 0;
+let yPos = 0;
+
+const turnClockwise = () => {
+  rotation += 90;
+  bug.style.transform = `rotate(${rotation}deg)`;
+};
+
+const turnAntiClockwise = () => {
+  rotation -= 90;
+  bug.style.transform = `rotate(${rotation}deg)`;
+};
+
+const setTransform = () => {
+  bug.style.left = `${xPos}px`;
+  bug.style.top = `${yPos}px`;
+};
+
+const stepLeft = () => {
+  xPos > 0 && (xPos -= 10);
+  setTransform();
+};
+
+const stepRight = () => {
+  xPos < 450 && (xPos += 10);
+  setTransform();
+};
+
+const stepUp = () => {
+  yPos > 0 && (yPos -= 10);
+  setTransform();
+};
+
+const stepDown = () => {
+  yPos < 450 && (yPos += 10);
+  setTransform();
+};
+
+const home = () => {
+  yPos = 0;
+  xPos = 0;
+  setTransform();
+};
+
+upBtn.addEventListener('click', stepUp);
+downBtn.addEventListener('click', stepDown);
+rightBtn.addEventListener('click', stepRight);
+leftBtn.addEventListener('click', stepLeft);
+homeBtn.addEventListener('click', home);
+turnClockwiseBtn.addEventListener('click', turnClockwise);
+turnAntiClockwiseBtn.addEventListener('click', turnAntiClockwise);
+
+//Keyboard events
+
+document.addEventListener('keyup', function (event) {
+  const { key } = event;
+  keyMapping[key]();
+});
+
+const keyMapping = {
+  ArrowUp: stepUp,
+  ArrowDown: stepDown,
+  ArrowLeft: stepLeft,
+  ArrowRight: stepRight,
+  ']': turnClockwise,
+  '[': turnAntiClockwise,
+  H: home,
+  h: home,
+};
+
