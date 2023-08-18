a5d8dfc5f54ef72a39a0e2b88f59e5dbd4c9566e
Prasenjit Das <prasenjitpriyan@gmail.com>
2023-08-18 22:42:10 +0530

    React deleted

--- React/1-Create-React-app/my-app/src/components/SubComponent.js
+++
@@ -1,9 +0,0 @@
-import './SubComponent.css'
-
-function SubComponent(){
-    return(
-      <p className="red">This is a subcomponent</p>  
-    )
-}
-
-export default SubComponent
--- React/1-Create-React-app/my-app/src/index.js
+++
@@ -1,8 +0,0 @@
-import ReactDom from "react-dom";
-
-import '/Users/prasenjitdas/Desktop/Codes/React/1-Create-React-app/my-app/src/index.css'
-
-import App from './App'
-
-
-ReactDom.render(<App/>, document.getElementById('root'));
